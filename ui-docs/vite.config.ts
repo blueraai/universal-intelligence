e74d38ca1d919a91baefed14efa5edbf97c00af2
Chris Braddock <braddock.chris@gmail.com>
2025-04-14 13:17:12 -0400

    allow dev host

--- ui-docs/vite.config.ts
+++ ui-docs/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // optional, in case you're routing via DNS or proxy
+    allowedHosts: ['dev.chrisbradd.io'], // <== allow your Ngrok host
+  },
 })
